131275758686896dc1daf8267f60d4e515bad753
이준호 <junho@ijunhoui-MacBookPro.local>
2023-02-28 18:55:30 +0900

    DB 이중화

--- app/Validators/OrderValidator.php
+++ app/Validators/OrderValidator.php
@@ -9,7 +9,7 @@ class OrderValidator
 {
     public static function postValidation(Request $request) {
         return Validator::make($request->all(), [
-            'code' => ['required','min:12','max:12','unique:orders'],
+            'code' => ['required','min:12','max:12','unique:mysql::write.orders'],
             'product_name' => ['required','max:100'],
         ]);
     }
--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -19,7 +19,7 @@ class UserValidator
             'name' => ['required','regex:/^[가-힣a-zA-Z\s]+$/','max:20'],
             'nickname' => ['required','regex:/^[a-z\s]+$/','max:30'],
             'password' => ['required','regex:/^.*(?=.{4,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).*$/','min:10','max:20'],
-            'email' => ['required','email','max:100','unique:users'],
+            'email' => ['required','email','max:100','unique:mysql::write.users'],
             'phone' => ['required','regex:/^[0-9\s]+$/','max:20'],
         ]);
     }
